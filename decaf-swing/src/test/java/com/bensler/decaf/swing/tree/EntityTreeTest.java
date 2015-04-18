70a90c827c515ccf36f207f0a3916b3efe13e980
Thomas Bensler <bensler@web.de>
2015-04-18 18:56:38 +0200

    using JGoodies LaF

--- decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
+++ decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
@@ -1,22 +1,29 @@
 package com.bensler.decaf.swing.tree;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.bensler.decaf.swing.view.PropertyViewImpl;
 import com.bensler.decaf.util.tree.Hierarchy;
+import com.jgoodies.plaf.plastic.Plastic3DLookAndFeel;
+import com.jgoodies.plaf.plastic.theme.DesertYellow;
 
 public class EntityTreeTest extends JFrame {
 
   private final EntityTree tree_;
 
   EntityTreeTest() {
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
     tree_ = new EntityTree(new PropertyViewImpl("name"));
     tree_.setData(createData());
     setContentPane(tree_.getComponent());
     setSize(500, 800);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws UnsupportedLookAndFeelException {
+    Plastic3DLookAndFeel.setCurrentTheme(new DesertYellow());
+    UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
     new EntityTreeTest().setVisible(true);
   }
 
