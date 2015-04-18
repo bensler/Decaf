493d249f12fd13e51912e90eaf52b5f71d81d06d
Thomas Bensler <bensler@web.de>
2015-04-18 18:31:55 +0200

    EntityTreeTest displays a tree ... hossa

--- decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
+++ decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
@@ -3,6 +3,7 @@ package com.bensler.decaf.swing.tree;
 import javax.swing.JFrame;
 
 import com.bensler.decaf.swing.view.PropertyViewImpl;
+import com.bensler.decaf.util.tree.Hierarchy;
 
 public class EntityTreeTest extends JFrame {
 
@@ -10,11 +11,28 @@ public class EntityTreeTest extends JFrame {
 
   EntityTreeTest() {
     tree_ = new EntityTree(new PropertyViewImpl("name"));
+    tree_.setData(createData());
+    setContentPane(tree_.getComponent());
     setSize(500, 800);
-
   }
+
   public static void main(String[] args) {
     new EntityTreeTest().setVisible(true);
+  }
+
+  private Hierarchy createData() {
+    final Hierarchy tree = new Hierarchy();
+
+    final Folder root = new Folder(null, "/");
+    final Folder home = new Folder(root, "home");
+    final Folder bobsHome = new Folder(home, "bob");
+    final Folder alicesHome = new Folder(home, "alice");
+
+    tree.add(alicesHome);
+    tree.add(bobsHome);
+    tree.add(home);
+    tree.add(root);
+    return tree;
   }
 
 }
