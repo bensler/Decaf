983bef8a1dbad73c0cf6eca525db8b97d046e50e
Thomas Bensler <thomas.bensler@zalando.de>
2015-04-24 12:16:18 +0200

    using FormLayout in EntityTreeTest

--- decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
+++ decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
@@ -1,9 +1,14 @@
 package com.bensler.decaf.swing.tree;
 
 import java.awt.Dialog.ModalityType;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -11,6 +16,8 @@ import org.junit.Test;
 
 import com.bensler.decaf.swing.view.PropertyViewImpl;
 import com.bensler.decaf.util.tree.Hierarchy;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
 import com.jgoodies.looks.plastic.theme.DesertYellow;
 
@@ -24,12 +31,27 @@ public class EntityTreeTest {
   @Test
   public void interactive() {
     final JDialog dialog = new JDialog(null, "Decaf Swing Test", ModalityType.APPLICATION_MODAL);
+    final JPanel panel = new JPanel(new FormLayout(
+      "3dlu, f:p:g, 3dlu",
+      "3dlu, f:p:g, 3dlu, p, 3dlu"
+    ));
     final EntityTree tree;
+    final JButton button;
 
     dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     tree = new EntityTree(new PropertyViewImpl("name"));
     tree.setData(createData());
-    dialog.setContentPane(tree.getComponent());
+    panel.add(new JScrollPane(tree.getComponent()), new CellConstraints(2, 2));
+    button = new JButton("Close");
+    button.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent evt) {
+        dialog.setVisible(false);
+      }
+    });
+    panel.add(button, new CellConstraints(2, 4, CellConstraints.RIGHT, CellConstraints.CENTER));
+
+    dialog.setContentPane(panel);
     dialog.setSize(500, 800);
     dialog.setVisible(true);
   }
