a00ac628fa28a2e3c3cf18fc122152f4fca4a5ef
Thomas Bensler <thomas.bensler@zalando.de>
2015-04-29 13:59:51 +0200

    UI testing with screenshot comparison

--- decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
+++ decaf-swing/src/test/java/com/bensler/decaf/swing/tree/EntityTreeTest.java
@@ -3,13 +3,14 @@ package com.bensler.decaf.swing.tree;
 import java.awt.AWTException;
 import java.awt.Component;
 import java.awt.Dialog.ModalityType;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -42,7 +43,7 @@ public class EntityTreeTest {
   }
 
   @Test
-  public void interactive() {
+  public void interactive() throws Exception {
     final JDialog dialog = new JDialog(null, "Decaf Swing Test", ModalityType.APPLICATION_MODAL);
     final JPanel panel = new JPanel(new FormLayout(
       "3dlu, f:p:g, 3dlu",
@@ -66,24 +67,25 @@ public class EntityTreeTest {
     dialog.setContentPane(panel);
     dialog.setSize(500, 800);
     dialog.setLocation(500,  100);
+
     new ScheduledThreadPoolExecutor(1).schedule(new Runnable() {
       @Override
       public void run() {
         SwingUtilities.invokeLater(new Runnable() {
           @Override
           public void run() {
-             Point location;
-
-
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            GraphicsDevice[] defaultScreen = ge.getScreenDevices();
-
-            GraphicsDevice dialogDevice = dialog.getGraphicsConfiguration().getDevice();
-
-            getScreenShot(dialog.getContentPane());
-            location = dialog.getLocationOnScreen();
+            getScreenShot(dialog.getContentPane(), "test_1.png");
             try {
-              new Robot().mouseMove((int)location.getX(), (int)location.getY());
+              final Point location = button.getLocationOnScreen();
+              final Dimension size = button.getSize();
+              final Robot bender = new Robot();
+
+              bender.mouseMove(
+                (int)(location.getX() + (size.getWidth() / 2)),
+                (int)(location.getY() + (size.getHeight() / 2))
+              );
+              bender.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+              bender.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
             } catch (AWTException e) {
               // TODO Auto-generated catch block
               e.printStackTrace();
@@ -95,17 +97,52 @@ public class EntityTreeTest {
     dialog.setVisible(true);
   }
 
-  void getScreenShot(Component component) {
+  void getScreenShot(Component component, String expected) {
     try {
-      final BufferedImage image = new BufferedImage(
+      final BufferedImage image = ImageIO.read(ImageIO.createImageInputStream(
+        ClassLoader.getSystemResourceAsStream(expected)
+      ));
+      final BufferedImage actual = new BufferedImage(
         component.getWidth(), component.getHeight(),
         BufferedImage.TYPE_INT_RGB
       );
-      component.paint( image.getGraphics() );
-      ImageIO.write(image, "PNG", new File("/home/tbensler/tmp/test.png"));
+      final BufferedImage diffImage;
+
+      component.paint(actual.getGraphics());
+      diffImage = diffImage(image, actual);
+      if (diffImage != null) {
+        ImageIO.write(diffImage, "PNG", new File("/home/tbensler/tmp/test.png"));
+      }
     } catch (IOException e) {
       throw new RuntimeException(e);
     };
+  }
+
+  private BufferedImage diffImage(BufferedImage img1, BufferedImage img2) {
+    final ColorModel cm1 = img1.getColorModel();
+    final ColorModel cm2 = img2.getColorModel();
+    final int width1 = img1.getWidth();
+    final int width2 = img2.getWidth();
+    final int height1 = img1.getHeight();
+    final int height2 = img2.getHeight();
+    final int width = Math.max(width1, width2);
+    final int height = Math.max(height1, height2);
+    final BufferedImage diffImg = new BufferedImage(
+      width, height, BufferedImage.TYPE_INT_RGB
+    );
+    boolean diff = false;
+
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < width; y++) {
+        final int rgb1 = img1.getRGB(x, y);
+        final int rgb2 = img2.getRGB(x, y);
+
+        if (rgb1 != rgb2) {
+          diff = true;
+        }
+      }
+    }
+    return (diff ? diffImg : null);
   }
 
   private Hierarchy<Folder> createData() {
