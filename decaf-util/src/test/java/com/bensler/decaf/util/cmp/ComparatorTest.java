aac68c7c7f3e85d717c2f40734096f94554afe2d
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-10-10 18:03:07 +0200

    NPE fixed

--- decaf-util/src/test/java/com/bensler/decaf/util/cmp/ComparatorTest.java
+++ decaf-util/src/test/java/com/bensler/decaf/util/cmp/ComparatorTest.java
@@ -8,13 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
-import com.google.common.collect.Lists;
 
 class ComparatorTest {
 
@@ -31,28 +30,37 @@ class ComparatorTest {
   }
 
   @Test
-  void nullSafe() {
-    final List<String> shuffled = Lists.newArrayList("x", null, "a", null, null, "z", "b");
-    Comparator<String> cmp;
-    List<String> sorted;
+  void nullsFirst() {
+    final List<String> shuffled = listOf("x", null, "a", null, null, "z", "b");
+    Comparator<String> uut = new NullSafeComparator<>(NULLS_FIRST, COLLATOR_COMPARATOR);
+    final List<String> sorted;
 
-    cmp = new NullSafeComparator<>(NULLS_FIRST, COLLATOR_COMPARATOR);
-    assertEquals(-1, cmp.compare(null, "a"));
-    assertEquals( 1, cmp.compare("a", null));
-    assertEquals( 0, cmp.compare(null, null));
-    assertEquals( 0, cmp.compare("a", "a"));
-    assertTrue(cmp.compare("a", "b") < 0);
-    Collections.sort(sorted = new ArrayList<>(shuffled), cmp);
-    assertEquals(Lists.newArrayList(null, null, null, "a", "b", "x", "z"), sorted);
+    assertEquals(-1, uut.compare(null, "a"));
+    assertEquals( 1, uut.compare("a", null));
+    assertEquals( 0, uut.compare(null, null));
+    assertEquals( 0, uut.compare("a", "a"));
+    assertTrue(uut.compare("a", "b") < 0);
+    Collections.sort(sorted = new ArrayList<>(shuffled), uut);
+    assertEquals(listOf(null, null, null, "a", "b", "x", "z"), sorted);
+  }
 
-    cmp = new NullSafeComparator<>(NULLS_LAST, COLLATOR_COMPARATOR);
-    assertEquals( 1, cmp.compare(null, "a"));
-    assertEquals(-1, cmp.compare("a", null));
-    assertEquals( 0, cmp.compare(null, null));
-    assertEquals( 0, cmp.compare("a", "a"));
-    assertTrue(cmp.compare("a", "b") < 0);
-    Collections.sort(sorted = new ArrayList<>(shuffled), cmp);
-    assertEquals(Lists.newArrayList("a", "b", "x", "z", null, null, null), sorted);
+  @Test
+  void nullsLast() {
+    final List<String> shuffled = listOf("x", null, "a", null, null, "z", "b");
+    Comparator<String> uut = new NullSafeComparator<>(NULLS_LAST, COLLATOR_COMPARATOR);
+    final List<String> sorted;
+
+    assertEquals( 1, uut.compare(null, "a"));
+    assertEquals(-1, uut.compare("a", null));
+    assertEquals( 0, uut.compare(null, null));
+    assertEquals( 0, uut.compare("a", "a"));
+    assertTrue(uut.compare("a", "b") < 0);
+    Collections.sort(sorted = new ArrayList<>(shuffled), uut);
+    assertEquals(listOf("a", "b", "x", "z", null, null, null), sorted);
+  }
+
+  private <E> List<E> listOf(E... es) {
+    return new ArrayList<>(Arrays.asList(es));
   }
 
 }
