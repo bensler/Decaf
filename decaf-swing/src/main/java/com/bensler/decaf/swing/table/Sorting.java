8e508f43634b3cc5ea9b01ae319a6340e3eb6691
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-10-10 18:18:07 +0200

    Guava dropped

--- decaf-swing/src/main/java/com/bensler/decaf/swing/table/Sorting.java
+++ decaf-swing/src/main/java/com/bensler/decaf/swing/table/Sorting.java
@@ -2,8 +2,6 @@ package com.bensler.decaf.swing.table;
 
 import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
-
 /** Represents a sort direction: {@link #ASCENDING}, {@link #DESCENDING} or {@link #NONE} .
  */
 public enum Sorting {
@@ -12,7 +10,7 @@ public enum Sorting {
   DESCENDING("descending", false, -1),
   NONE("none", false, 0);
 
-  public final static Map<Sorting, Sorting> OPPOSITES = ImmutableMap.of(
+  public final static Map<Sorting, Sorting> OPPOSITES = Map.of(
     ASCENDING,  DESCENDING,
     DESCENDING, ASCENDING,
     NONE,       NONE
