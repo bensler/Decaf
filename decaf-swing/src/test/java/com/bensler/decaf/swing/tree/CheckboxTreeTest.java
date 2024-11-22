624527e066f241e3ecf759851528bb13fec994ce
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-11-22 13:57:19 +0100

    CheckboxTreeTest added

---
+++ decaf-swing/src/test/java/com/bensler/decaf/swing/tree/CheckboxTreeTest.java
@@ -0,0 +1,33 @@
+package com.bensler.decaf.swing.tree;
+
+import java.awt.AWTException;
+import java.awt.Point;
+
+import org.junit.jupiter.api.Test;
+
+import com.bensler.decaf.testutil.Bender;
+import com.bensler.decaf.testutil.TestImageSample;
+
+class CheckboxTreeTest {
+
+  private final Bender bender_;
+
+  public CheckboxTreeTest() throws AWTException {
+    bender_ = new Bender("target/surefire-reports");
+  }
+
+  @Test
+  void interactive() throws Exception {
+    final CheckboxTreeDialog app = new CheckboxTreeDialog();
+
+    app.dialog_.setLocation(bender_.getLargestScreensOrigin());
+    app.dialog_.setVisible(true);
+    bender_.clickOn(app.cbTree_.getScrollPane(), new Point(88,  93));
+    bender_.clickOn(app.cbTree_.getScrollPane(), new Point(68,  244));
+    bender_.clickOn(app.cbTree_.getScrollPane(), new Point(49,  229));
+    bender_.clickOn(app.cbTree_.getScrollPane(), new Point(28,  196));
+    bender_.assertEqualsVisually(app.dialog_.getContentPane(), new TestImageSample());
+    bender_.waitForAllTasksCompleted();
+  }
+
+}
