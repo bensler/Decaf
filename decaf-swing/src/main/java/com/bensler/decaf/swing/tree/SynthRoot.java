e267f98a6b688c2817d495572cd95fa5bb9ea479
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-11-24 13:15:49 +0100

    JavaDoc added in SynthRoot

--- decaf-swing/src/main/java/com/bensler/decaf/swing/tree/SynthRoot.java
+++ decaf-swing/src/main/java/com/bensler/decaf/swing/tree/SynthRoot.java
@@ -1,7 +1,10 @@
 package com.bensler.decaf.swing.tree;
 
 import com.bensler.decaf.util.tree.Hierarchical;
+import com.bensler.decaf.util.tree.Hierarchy;
 
+/** Root object which is nerver visible but exists to have a common root node above potentially many
+ * root nodes of a {@link Hierarchy}  */
 public final class SynthRoot<H> extends Object implements Hierarchical<H> {
 
   SynthRoot() {}
