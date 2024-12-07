961950a89db6c8d6e7771f636e934a5ea0b766cd
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-12-07 22:24:05 +0100

    Hierarchical.toPath added

--- decaf-util/src/main/java/com/bensler/decaf/util/tree/Hierarchical.java
+++ decaf-util/src/main/java/com/bensler/decaf/util/tree/Hierarchical.java
@@ -1,5 +1,8 @@
 package com.bensler.decaf.util.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Implemented by classes being in a hierarchical order, which means they have a parent. There is no assumption made
  * about children. This is possible only in the context of a Hierarchy. So one Hierarchical might be a member of
@@ -8,5 +11,14 @@ package com.bensler.decaf.util.tree;
 public interface Hierarchical <P> {
 
     P getParent();
+
+    public static <H extends Hierarchical<H>> List<H> toPath(H node) {
+      final LinkedList<H> path = new LinkedList<>();
+
+      do {
+        path.addFirst(node);
+      } while((node = node.getParent()) != null);
+      return path;
+    }
 
 }
