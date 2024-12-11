cef1e6c0b70a65cf1439c85d684a8963fcbc7668
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-12-11 10:52:12 +0100

    basic content validation infrastructure added in BasicContentPanel

--- decaf-swing/src/main/java/com/bensler/decaf/swing/dialog/BasicContentPanel.java
+++ decaf-swing/src/main/java/com/bensler/decaf/swing/dialog/BasicContentPanel.java
@@ -2,7 +2,11 @@ package com.bensler.decaf.swing.dialog;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
+import com.bensler.decaf.swing.EntityComponent;
 import com.jgoodies.forms.layout.FormLayout;
 
 public abstract class BasicContentPanel<IN, OUT> extends JPanel implements ContentPanel<IN, OUT> {
@@ -28,6 +32,36 @@ public abstract class BasicContentPanel<IN, OUT> extends JPanel implements Conte
   @Override
   public final void setInData(IN inData) {
     setData(inData_ = inData);
+    validate(null);
+  }
+
+  protected abstract boolean validateContent(Object eventSource);
+
+  protected void addValidationSource(EntityComponent<?> entityComponent) {
+    entityComponent.setSelectionListener((source, selection) -> validate(entityComponent));
+  }
+
+  protected void addValidationSource(JTextField textfield) {
+    textfield.getDocument().addDocumentListener(new DocumentListener() {
+      @Override
+      public void removeUpdate(DocumentEvent e) {
+        validate(textfield);
+      }
+
+      @Override
+      public void insertUpdate(DocumentEvent e) {
+        validate(textfield);
+      }
+
+      @Override
+      public void changedUpdate(DocumentEvent e) {
+        validate(textfield);
+      }
+    });
+  }
+
+  void validate(Object validationSource) {
+    ctx_.setValid(validateContent(validationSource));
   }
 
 }
