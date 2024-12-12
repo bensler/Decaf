e3e4b27627555a7455f440cd7a31198171d29ee1
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-12-12 13:44:41 +0100

    headerPanel introduced

--- decaf-swing/src/main/java/com/bensler/decaf/swing/dialog/OkCancelDialog.java
+++ decaf-swing/src/main/java/com/bensler/decaf/swing/dialog/OkCancelDialog.java
@@ -1,21 +1,27 @@
 package com.bensler.decaf.swing.dialog;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.Window;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import com.bensler.decaf.swing.awt.IconComponent;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
 public class OkCancelDialog<IN, OUT> extends JDialog implements ContentPanel.Context {
 
   private final JButton okButton_;
+
+  private final JPanel headerPanel_;
 
   private final ContentPanel<IN, OUT> contentPanel_;
 
@@ -27,12 +33,29 @@ public class OkCancelDialog<IN, OUT> extends JDialog implements ContentPanel.Con
     final CellConstraints cc = new CellConstraints();
     final JPanel mainPanel = new JPanel(new FormLayout(
       "3dlu, f:p:g, 3dlu",
-      "3dlu, f:p:g, 3dlu, f:p, 3dlu"
+      "3dlu, f:p, 3dlu, f:p:g, 3dlu, f:p, 3dlu"
     ));
 
-    mainPanel.add((contentPanel_ = contentPanel).getComponent(this), cc.xy(2, 2));
-    mainPanel.add(createButtonPanel(okButton_ = new JButton("Ok")), cc.xy(2, 4));
+    mainPanel.add((headerPanel_ = createHeaderPanel(appearance)), cc.xy(2, 2));
+    mainPanel.add((contentPanel_ = contentPanel).getComponent(this), cc.xy(2, 4));
+    mainPanel.add(createButtonPanel(okButton_ = new JButton("Ok")), cc.xy(2, 6));
     setContentPane(mainPanel);
+  }
+
+  private JPanel createHeaderPanel(DialogAppearance appearance) {
+    final JLabel titleLabel = new JLabel(appearance.getTitle());
+    final Font titleFont = titleLabel.getFont();
+    final CellConstraints cc = new CellConstraints();
+    final JPanel headerPanel = new JPanel(new FormLayout(
+      "3dlu, f:p:g, 3dlu, f:p, 3dlu",
+      "3dlu, f:p, 3dlu"
+    ));
+
+    titleLabel.setFont(titleFont.deriveFont(titleFont.getSize() * 1.3f).deriveFont(Font.BOLD));
+    headerPanel.add(titleLabel, cc.xy(2, 2, "l, c"));
+    headerPanel.add(new IconComponent(appearance.getIcon()), cc.xy(4, 2));
+    headerPanel.setBackground(Color.WHITE);
+    return headerPanel;
   }
 
   private JPanel createButtonPanel(JButton okButton) {
