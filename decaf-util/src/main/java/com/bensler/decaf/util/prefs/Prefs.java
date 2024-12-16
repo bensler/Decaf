ff536ffd6a7e95cb200626337a6b1c939f0e0262
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-12-16 08:57:26 +0100

    Prefs class and dtd resource introduced

---
+++ decaf-util/src/main/java/com/bensler/decaf/util/prefs/Prefs.java
@@ -0,0 +1,9 @@
+package com.bensler.decaf.util.prefs;
+
+public class Prefs {
+
+  public static final String DTD_SYSTEM_ID_1_0 = "com.bensler.taggy.Prefs.1.0";
+
+  public static final String DTD_RESOURCE_1_0 = "prefs-1.0.dtd";
+
+}
