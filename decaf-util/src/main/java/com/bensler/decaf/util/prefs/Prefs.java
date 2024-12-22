a0528025641cfc21ac855bd151c28b87c382b16f
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-12-22 12:03:52 +0100

    prefs DTD ID changed

--- decaf-util/src/main/java/com/bensler/decaf/util/prefs/Prefs.java
+++ decaf-util/src/main/java/com/bensler/decaf/util/prefs/Prefs.java
@@ -36,7 +36,7 @@ import com.bensler.decaf.util.tree.Hierarchy;
 
 public class Prefs {
 
-  public static final String DTD_SYSTEM_ID_1_0 = "com.bensler.taggy.Prefs.1.0";
+  public static final String DTD_SYSTEM_ID_1_0 = "com.bensler.decaf.Prefs.1.0";
 
   public static final String DTD_RESOURCE_1_0 = "prefs-1.0.dtd";
 
