4f5cdcdccbd233d9070a1113c54fcf5d6cacce79
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2025-01-10 20:28:34 +0100

    rendering strategy refined

--- decaf-swing/src/main/java/com/bensler/decaf/swing/view/SimpleCellRenderer.java
+++ decaf-swing/src/main/java/com/bensler/decaf/swing/view/SimpleCellRenderer.java
@@ -1,5 +1,7 @@
 package com.bensler.decaf.swing.view;
 
+import java.util.Optional;
+
 import javax.swing.Icon;
 import javax.swing.JLabel;
 
@@ -20,15 +22,12 @@ public class SimpleCellRenderer extends Object implements CellRenderer {
   /** Sets the text to <code>value.toString()</code>.
    * @param cellValue <code>null</code> allowed.
    */
-  public boolean setText(JLabel comp, Object cellValue) {
-    final String text = renderString_(cellValue);
-
-    comp.setText(text);
-    return !text.trim().isEmpty();
+  public void setText(JLabel comp, Object cellValue) {
+    comp.setText(renderString_(cellValue));
   }
 
   private String renderString_(Object cellValue) {
-    return ((cellValue == null) ? " " : renderString(cellValue));
+    return Optional.ofNullable(cellValue).map(this::renderString).orElse(" ");
   }
 
   protected String renderString(Object cellValue) {
@@ -37,11 +36,9 @@ public class SimpleCellRenderer extends Object implements CellRenderer {
 
   @Override
   public void render(Object viewable, Object cellValue, JLabel comp) {
-    Icon icon = null;
+    final Icon icon = getIcon(viewable, cellValue);
 
-    if (setText(comp, cellValue)) {
-      icon = getIcon(viewable, cellValue);
-    }
+    setText(comp, cellValue);
     comp.setIcon(icon);
     comp.setIconTextGap((icon != null) ? TEXT_ICON_GAP : 0);
     comp.setHorizontalAlignment(JLabel.LEFT);
