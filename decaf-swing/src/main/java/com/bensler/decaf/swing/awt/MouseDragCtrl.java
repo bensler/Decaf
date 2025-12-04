56bbb87e77390585489560cd0ab91e6d480161bb
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2025-12-04 17:13:05 +0100

    MouseDragCtrl added

---
+++ decaf-swing/src/main/java/com/bensler/decaf/swing/awt/MouseDragCtrl.java
@@ -0,0 +1,37 @@
+package com.bensler.decaf.swing.awt;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+public class MouseDragCtrl extends MouseAdapter {
+
+  private final BiConsumer<Point, Point> dragListener_;
+  private Optional<Point> origin_;
+
+  public MouseDragCtrl(Component target, BiConsumer<Point, Point> dragListener) {
+    dragListener_ = dragListener;
+    origin_ = Optional.empty();
+    target.addMouseListener(this);
+    target.addMouseMotionListener(this);
+  }
+
+  @Override
+  public void mousePressed(MouseEvent evt) {
+    origin_ = Optional.of(evt.getPoint());
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent evt) {
+    origin_ = Optional.empty();
+  }
+
+  @Override
+  public void mouseDragged(MouseEvent evt) {
+    origin_.ifPresent(origin -> dragListener_.accept(origin, evt.getPoint()));
+  }
+
+}
